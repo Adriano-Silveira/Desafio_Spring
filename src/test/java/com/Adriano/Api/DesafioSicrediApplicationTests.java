652c57a535a33ce0d26e7a31468370808c62ec42
Adriano-Silveira <73249347+Adriano-Silveira@users.noreply.github.com>
2021-02-11 15:01:36 -0300

    Primer commit de archivos

---
+++ src/test/java/com/Adriano/Api/DesafioSicrediApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Adriano.Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioSicrediApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
